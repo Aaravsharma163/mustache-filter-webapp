f6a40346da324f84edc9cd009cb240ea28081c3f
Aaravsharma163 <84375040+Aaravsharma163@users.noreply.github.com>
2021-12-10 14:57:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300,280);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('myfilterimg.png');
+}
